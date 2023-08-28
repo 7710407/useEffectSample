956226b6c0689bbdec28d995b50c28fb3a640118
7710407 <7710407@gmail.com>
2023-08-28 09:58:01 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [id, setId] = useState("")
+  const [data, setData] = useState()
+  useEffect(() => {
+    if (id.length === 2) {
+      let stream = fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(stream => stream.json())
+      .then(results => { 
+        setData(results)
+        })
+      // let text2 = setTitle(results))
+    }
+  }, [id])
+
+  let title = ""
+  let todoId = ""
+  if (data !== undefined) {
+    title = data.title
+    todoId = data.id
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{id}</h1>
+      <input
+        type='text'
+        value={id}
+        onChange={(evt) => setId(evt.target.value)}
+      />
+
+      <h1>
+        ## {todoId}  {title} ##
+      </h1>
     </div>
   );
 }
